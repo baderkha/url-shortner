2a656e890e3461ffbc951f4ab416aab0436032cd
ahmad <ahmad.baderkhan@youvisit.com>
2020-10-04 15:30:10 -0400

    added env json

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"url-shortner/dependency"
 )
 
+type ServerEnv struct {
+	DSN string `json:"dsn"`
+}
+
 func main() {
+	jsonFile, err := os.Open("env.json")
+	if err != nil {
+		panic(":0 PANIK , enviroment json not found")
+	}
+	byteValue, _ := ioutil.ReadAll(jsonFile)
 	dsn := "root:password@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
